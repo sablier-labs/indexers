b5cd366ff209499501360cf1d08eba65130758e9
Paul Razvan Berg <prberg@proton.me>
2025-08-22 19:23:09 +0300

    chore: remove analytics bindings

--- envio/analytics/bindings/src/GqlDbCustomTypes.gen.ts
+++
@@ -1,8 +0,0 @@
-/* TypeScript file generated from GqlDbCustomTypes.res by genType. */
-
-/* eslint-disable */
-/* tslint:disable */
-
-export type Float_t = number;
-
-export type Int_t = number;
--- envio/analytics/bindings/src/db/DbFunctions.gen.ts
+++
@@ -1,8 +0,0 @@
-/* TypeScript file generated from DbFunctions.res by genType. */
-
-/* eslint-disable */
-/* tslint:disable */
-
-import type {EventSyncState_t as TablesStatic_EventSyncState_t} from './TablesStatic.gen';
-
-export type EventSyncState_eventSyncState = TablesStatic_EventSyncState_t;
